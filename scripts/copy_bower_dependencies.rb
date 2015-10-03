1003e7b9e8e192ed9068c3c4a93e882672d520a4
David Herman <d9n.coding@gmail.com>
2015-10-02 22:40:26 -0700

    Add bower pygments for more highlight color choices

--- scripts/copy_bower_dependencies.rb
+++ scripts/copy_bower_dependencies.rb
@@ -11,6 +11,8 @@ sass_dir = '_sass/third-party'
 FileUtils.mkdir(sass_dir) if !File.exists?(sass_dir)
 FileUtils.cp('bower_components/gridism/gridism.css', "#{sass_dir}/_gridism.scss")
 FileUtils.cp('bower_components/normalize-css/normalize.css', "#{sass_dir}/_normalize.scss")
+FileUtils.cp('bower_components/pygments/css/igor.css',
+"#{sass_dir}/_syntax-highlighting.scss")
 
 fa_dir = "#{sass_dir}/font-awesome" # working directory, will remove later
 FileUtils.mkdir(fa_dir) if !File.exists?(fa_dir)
