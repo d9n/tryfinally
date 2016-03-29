b19b6cffadf3ae78eb8849b0df27d84caca10475
David Herman <d9n.coding@gmail.com>
2016-03-28 22:11:21 -0700

    Table of contents plugin now handles two levels deep

--- _plugins/filters/toc.rb
+++ _plugins/filters/toc.rb
@@ -9,10 +9,14 @@
 # {{ content }}
 # </div>
 
+# HTML content that looks like this...
+# <h2>Section 1</h2>
+# <h2>Section 2</h2>
+#
 # Generates results that look like
 # <ul>
-#    <li><a href="...">Link to section 1</a></li>
-#    <li><a href="...">Link to section 2</a></li>
+#    <li><a href="...">Section 1</a></li>
+#    <li><a href="...">Section 2</a></li>
 # </ul>
 #
 # So wrap in a css div and style as you wish!
@@ -20,22 +24,31 @@
 module Jekyll
   module TocFilter
     def toc(input)
-        headers = input.scan(/<h2.+<\/h2>/)
-        nameIds = []
-        for header in headers
-            match = /<h2 .*id="\S+".*>(.+)<a href="(\S+)".*/.match(header)
-            if match
-                nameIds.push(match.captures)
-            end
-        end
+        headers = input.scan(/<h\d.+<\/h\d>/)
+        headerRegex = /<h([23]) .*id="\S+".*>(.+)<a href="(\S+)".*/
 
-        if nameIds.empty?
-            return '(Missing TOC, please add h2 sections in your content)'
+        matches = headers.map { |h| headerRegex.match(h) }
+        matches.compact!
+
+        # If any h3 section comes before h2 sections drop them for being out of order
+        matches = matches.drop_while { |m| m[1] == '3' }
+
+        if matches.empty?
+            return '(Missing TOC, please add h2/h3 sections in your content)'
         end
 
         output = '<ul>'
-        for nameId in nameIds
-            output << "<li><a href=\"#{nameId[1]}\">#{nameId[0]}</a></li>"
+        lastDepth = 0;
+        for match in matches
+            if match[1] == '3' and lastDepth == 0
+                output << '<ul>'
+                lastDepth = 1
+            elsif match[1] == '2' and lastDepth == 1
+                output << '</ul>'
+                lastDepth = 0
+            end
+
+            output << "<li><a href=\"#{match[3]}\">#{match[2]}</a></li>"
         end
         output << '</ul>'
     end
